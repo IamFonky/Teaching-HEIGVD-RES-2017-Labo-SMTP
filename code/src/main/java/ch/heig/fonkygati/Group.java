d2615d4f009ce7a6fbeb66fa322f4837289045a4
Gaetan Othenin-Girard <gaetan.othenin-girard@heig-vd.ch>
2017-04-06 22:38:01 +0200

    Commenting finished !

--- code/src/main/java/ch/heig/fonkygati/Group.java
+++ code/src/main/java/ch/heig/fonkygati/Group.java
@@ -8,38 +8,61 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by Fonky on 04.04.2017.
+ * Class representing a group of victims used in the "Prank Compain". It contains victims senders and receivers.
  */
 public class Group
 {
-
+   // Lists of senders and receivers victims
    private String[] senders;
    private String[] receivers;
 
+   /**
+    * Default constructor for Group.
+    */
    public Group() {}
 
+   /**
+    * Construct a Group with given senders and receivers victims as strings.
+    * @param senders array of senders victims as strings.
+    * @param receivers array of receivers victims as strings.
+    */
    public Group(String[] senders, String[] receivers)
    {
       this.senders = senders;
       this.receivers = receivers;
    }
 
+   /**
+    * Give the list of senders victims.
+    * @return array of strings representing the emails of the senders.
+    */
    public String[] getSenders()
    {
       return senders;
    }
 
+   /**
+    * Set the list of senders victims.
+    * @param senders array of strings representing the emails of the senders.
+    */
    public void setSenders(String[] senders)
    {
       this.senders = senders;
    }
 
-
+   /**
+    * Give the list of receivers victims.
+    * @return array of strings representing emails of the receivers.
+    */
    public String[] getReceivers()
    {
       return receivers;
    }
 
+   /**
+    * Set the list of receivers victims.
+    * @param receivers array of strings representing emails of the receivers.
+    */
    public void setReceivers(String[] receivers)
    {
       this.receivers = receivers;
--- code/src/main/java/ch/heig/fonkygati/OurSmtpClient.java
+++ code/src/main/java/ch/heig/fonkygati/OurSmtpClient.java
@@ -4,27 +4,42 @@ import java.io.*;
 import java.net.Socket;
 import java.util.*;
 
+/**
+ * Program that play a "Prank Campain" and send pranks emails from senders victims to receivers victims. It uses files
+ * that contain series of predefined prank messages and list of victims.
+ *
+ * @author Pierre-Benjamin Monaco (IamFonky)
+ * @author Gaëtan Othenin-Girard (GOthGir)
+ */
 public class OurSmtpClient
 {
+   // Tokens used in the logs to specify who sent the displayed message
    private final static String CLIENT_P = "Client  ~~o ";
    private final static String SERVER_P = "Serveur ~~o ";
 
+   // Predefined separator used in the victims and pranks files to indicate the end of a data
    private final static String END_OF_DATA_LOL = "!$&#%#&$!";
 
+   // Predefined separator used to say to the server that we have finished to send datas
    private final static String END_OF_DATA = "\r\n.\r\n";
 
+   // Informations about the SMTP server we want to connect
    private static String name = "fonkygati";
    private static String host = "localhost";
    private static int port = 2525;
 
+   // HashSets containing the groups of victims and the prank texts
    private static HashSet<Group> groups;
    private static HashSet<String> pranks;
 
+   // Socket connected to the SMTP server
    private static Socket socket;
 
+   // Reader and Writer used to communicate with the SMTP server
    private static SmtpWriter writer;
    private static BufferedReader reader;
 
+   // Random generator used to chose a random prank
    private static Random rand;
 
 
@@ -37,7 +52,7 @@ public class OurSmtpClient
     */
    public static void main(String[] args)
    {
-      //Check for args
+      // Check for args
       if(args.length < 1)
       {
          System.out.println("Le programme à besoin d'arguments pour démarrer." +
@@ -60,33 +75,33 @@ public class OurSmtpClient
             // We get the current command
             String command = args[i];
 
-            //Executes the corresponding action or displays the help
+            // Executes the corresponding action or displays the help
             if (command.equals("-victims") && args.length > (i + 1))
             {
-               //Fetches victims in victim file
+               // Fetches victims in victim file
                fetchVictims(argument);
                i++;
             } else if (command.equals("-pranks") && args.length > (i + 1))
             {
-               //Fetches pranks in victim pranks
+               // Fetches pranks in victim pranks
                fetchPranks(argument);
                i++;
             }
             else if(command.equals("-address") && args.length > (i + 1))
             {
-               //Sets the host address
+               // Sets the host address
                host = args[i+1];
                i++;
             }
             else if(command.equals("-port") && args.length > (i + 1))
             {
-               //Sets the port number
+               // Sets the port number
                port = Integer.valueOf(args[i+1]);
                i++;
             }
             else
             {
-               //Displays help!
+               // Displays help!
                System.out.println("Le programme possède quatres commandes (dans n'importe quel ordre) : ");
                System.out.println("-victim  (obligatoire)      : chemin vers le fichier contenant les groupes de victimes");
                System.out.println("-pranks  (obligatoire)      : chemin vers le fichier contenant les blagues");
@@ -104,8 +119,11 @@ public class OurSmtpClient
          }
       }
 
+      // Connect to the SMTP server
       connect();
+      // Send the prank messages
       groupSend();
+      // Disconnect from the SMTP server
       disconnect();
    }
 
@@ -139,23 +157,30 @@ public class OurSmtpClient
       }
    }
 
+   /**
+    * Method that takes a victim file and fetch the email addresses.
+    * @param path path to the victim file.
+    * @throws IOException throws an exception if the file doesn't exist.
+    */
    private static void fetchVictims(String path) throws IOException
    {
+      // Read the victim file
       reader = new BufferedReader(new FileReader(path));
       groups = new HashSet<>();
       String jsonGroupLine;
       String jsonGroup = "";
-      // For each line from the file, collect the victims' mail
+
+      // For each line from the file, collect the victims' email adresse
       while ((jsonGroupLine = reader.readLine()) != null)
       {
-         // We clean the line by removing the END_OF_DATA_LOL separator
+         // We clean the line by removing the end of data separator
          String[] cleanLine = jsonGroupLine.split(END_OF_DATA_LOL);
          if(cleanLine.length > 0)
          {
             jsonGroup += cleanLine[0];
          }
 
-         // We
+         // We parse que line and add the victims to the group
          if(jsonGroupLine.indexOf(END_OF_DATA_LOL) != -1)
          {
             groups.add(JsonObjectMapper.parseJson(jsonGroup, Group.class));
@@ -165,15 +190,26 @@ public class OurSmtpClient
       reader.close();
    }
 
+   /**
+    * Method that takes a prank file and fetch the prank texts.
+    * @param path path to the prank file.
+    * @throws IOException throws an exception if the file doesn't exist.
+    */
    private static void fetchPranks(String path) throws IOException
    {
+      // Read the prank file
       reader = new BufferedReader(new FileReader(path));
       pranks = new HashSet<>();
       String prankLine;
       String prank = "";
+
+      // For each line, we get the prank text
       while ((prankLine = reader.readLine()) != null)
       {
+         // We clean the line by removing the end of data separator
          String[] cleanLine = prankLine.split(END_OF_DATA_LOL);
+         // If the line doesn't contain an end of data separator, we add a end of line separator to the prank text
+         // Otherwise, the prank text is finished and we add the prank to the pranks list
          if(cleanLine.length > 0 && !cleanLine[0].equals(END_OF_DATA_LOL))
          {
             prank += cleanLine[0] + "\r\n";
@@ -187,20 +223,29 @@ public class OurSmtpClient
       reader.close();
    }
 
+   /**
+    * Method that send a message to a group of victims with a predefined subject (Let's LOL).
+    */
    private static void groupSend()
    {
       groupSend("Let's LOL");
    }
 
+   /**
+    * Method that send a prank message to a group of victims with a given subject.
+    * @param subject the message's subject.
+    */
    private static void groupSend(String subject)
    {
       // Initalize random to choose random pranks
       rand = new Random();
 
+      // For each group of victims, we send them a prank message from each sender victims chosen
       for (Group group : groups)
       {
          for (String sender : group.getSenders())
          {
+            // Sand the mail with a random selected prank text
             sendMail(
                     sender,
                     subject,
@@ -267,13 +312,18 @@ public class OurSmtpClient
    private static List<String> makeHeader(String emailFrom, String subject,String... emailsTo)
    {
       List<String> header = new ArrayList<String>();
+      // Add the sender
       header.add("From: " + emailFrom);
+      // Add all the receivers
       for (int i = 0; i < emailsTo.length; ++i)
       {
          header.add((i == 0) ? "To: " : "Cc: " + emailsTo[i]);
       }
+      // Add the mail's subject
       header.add("Subject: " + subject);
       header.add("");
+
+      // Return the formated header for the mail
       return header;
    }
 
@@ -301,13 +351,15 @@ public class OurSmtpClient
    }
 
    /**
-    * Method used to write to the server.
-    * @param lines strings we want to write to the server.
+    * Method used to write lines to the server.
+    * @param lines strings sent as lines that we want to write to the server.
     */
    private static void sendAndFlush(String... lines)
    {
+      // We send line by line
       for (String line : lines)
       {
+         // Show the client message in the logs
          System.out.println(CLIENT_P + line);
          writer.println(line);
 
@@ -315,6 +367,9 @@ public class OurSmtpClient
       writer.flush();
    }
 
+   /**
+    * Method used to disconnect from the server.
+    */
    private static void disconnect()
    {
       try
